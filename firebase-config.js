a92d72eaf02e014ea3300aa04a0677d0d816eb53
Kencheese12 <hamakenhome@gmail.com>
2026-08-29 14:03:13 +0900

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+// ==========================================================
+// Firebaseの設定ファイル
+// README.md の手順にそって、Firebaseコンソールで取得した
+// 「あなたの」設定値に、下の7つを書き換えてください。
+// ==========================================================
+
+export const firebaseConfig = {
+  apiKey: "ここにapiKeyを貼り付け",
+  authDomain: "ここにauthDomainを貼り付け",
+  databaseURL: "ここにdatabaseURLを貼り付け",
+  projectId: "ここにprojectIdを貼り付け",
+  storageBucket: "ここにstorageBucketを貼り付け",
+  messagingSenderId: "ここにmessagingSenderIdを貼り付け",
+  appId: "ここにappIdを貼り付け"
+};
