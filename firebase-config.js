567582472d5770ce22df51f6c9aee52f4982dd4b
Kencheese12 <hamakenhome@gmail.com>
2026-09-05 11:52:11 +0900

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -5,11 +5,11 @@
 // ==========================================================
 
 export const firebaseConfig = {
-  apiKey: "ここにapiKeyを貼り付け",
-  authDomain: "ここにauthDomainを貼り付け",
-  databaseURL: "ここにdatabaseURLを貼り付け",
-  projectId: "ここにprojectIdを貼り付け",
-  storageBucket: "ここにstorageBucketを貼り付け",
-  messagingSenderId: "ここにmessagingSenderIdを貼り付け",
-  appId: "ここにappIdを貼り付け"
+  apiKey: "AIzaSyBysnu3NoYr0V5rkDZTKaZD2SUWuzvFyGo",
+  authDomain: "order-system-a549f.firebaseapp.com",
+  databaseURL: "https://order-system-a549f-default-rtdb.firebaseio.com",
+  projectId: "order-system-a549f",
+  storageBucket: "order-system-a549f.firebasestorage.app",
+  messagingSenderId: "223807805608",
+  appId: "1:223807805608:web:24be53b011211bb22f8a9d",
 };
